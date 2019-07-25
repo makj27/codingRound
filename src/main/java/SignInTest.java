26eb7c1756860103ef6311557cf994c5087fc002
Mayank Jain <GS-1992@GSLAB.COM>
2019-07-25 08:43:10 +0530

    update SignInTest

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,30 +1,51 @@
-import com.sun.javafx.PlatformUtil;
+//import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.core.StringStartsWith.startsWith;
 
 public class SignInTest {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+    @BeforeTest
+    public void setup(){
+        setDriverPath();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterSuite
+    public void tearDown() throws InterruptedException {
+        if(driver != null) {
+            driver.close();
+            driver.quit();
+            driver=null;
+        }
+    }
 
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
-
-        setDriverPath();
 
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
 
+        driver.findElement(By.id("SignIn")).click();
+        waitFor(2000);
+        driver.switchTo().frame("modal_window");
         driver.findElement(By.id("signInButton")).click();
 
         String errors1 = driver.findElement(By.id("errors1")).getText();
+        driver.switchTo().defaultContent();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
+
     }
 
     private void waitFor(int durationInMilliSeconds) {
@@ -36,13 +57,13 @@ public class SignInTest {
     }
 
     private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
+        if (System.getProperty("os.name").startsWith("Mac")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
-        if (PlatformUtil.isWindows()) {
+        if (System.getProperty("os.name").startsWith("Windows")){
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
-        if (PlatformUtil.isLinux()) {
+        if (System.getProperty("os.name").startsWith("Linux")){
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
     }
